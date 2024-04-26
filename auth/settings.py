770698aa0458de218d69ee45d897d26be4944440
malak-lawanseh <malaklawanseh0@gmail.com>
2024-04-26 16:32:10 +0000

    cookies

--- auth/settings.py
+++ auth/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["*","localhost:3000"]    #,"localhost:8000"
 CORS_ALLOWED_ORIGINS = [
     # "http://localhost:8000",  
     "http://localhost:3000",  
+    "http://127.0.0.1:3000"
 ]
 
 # Application definition
